bd58e569435304a191a1fd0a2621e81151db028b
Joël Henri <ebosqua.edimo@gmail.com>
2023-11-03 17:37:12 +0100

    Définition routes principales

--- application.js
+++ application.js
@@ -9,17 +9,45 @@ app.use(express.json())
 // Encoder l'url
 app.use(express.urlencoded())
 
-// Définir le chemin de l'API
-app.get("/api/justify", (req, res) => {
+// Définition des chemins de d'authentification 
+
+app.get("/",(req,res) => {
+    res.sendFile("./authenticate.html", { root: __dirname})
+})
+
+app.post("/", (req, res) => {
+        if(req.body.email){
+            // Génération de token
+            // Redirection vers la page d'insertion
+            res.header("Location","/insert").status(302).end()
+        }
+        console.log("Connectez-vous")
+        res.end()
+})
+
+
+// Définition des routes de l'API
+app.get("/insert", (req, res) => {
     res.sendFile("./insertion.html", { root: __dirname})
     // res.end()
 })
 
 // Définir la route pour le traitement du formulaire
-app.post("/api/justify", (req, res) => {
-    console.log(req.body)
-    res.header("Location",req.url).status(302).end()
+app.post("/api/:chemin", (req, res) => {
+    if(req.params.chemin === "justify"){
+        console.log(req.body)
+        // Traitement de justification
+        // res.header("Location",req.url).status(302).send("Texte justifié")
+        res.header("content-type","text/plain").status(200).send(req.body.text)
+    }
+    if(req.params.chemin === "token"){
+        console.log(req.body)
+        // Traitement de justification
+        // res.header("Location",req.url).status(302).send("Texte justifié")
+        res.header("content-type","application/json").status(200).send(req.body)
+    }
 })
+
 
 // Engager l'écoute des requêtes/ réponses faites au serveur
 app.listen(2500,() => {
