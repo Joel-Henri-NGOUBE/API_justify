7c7c65a94f4b9c3e5043a21987fcf3eb6217280e
Joël Henri <ebosqua.edimo@gmail.com>
2023-11-06 14:19:56 +0100

    Logique Justification début

--- application.js
+++ application.js
@@ -1,10 +1,24 @@
 // Importation d'express.js pour faciliter l'implémentation du serveur.
 const express = require("express")
 
+const mysql = require("mysql")
+
+const connection = require("express-myconnection")
+
+const connectOptions = {
+    host : "localhost",
+    user : "root",
+    password : "",
+    port : 3306,
+    database : "justify"
+}
+
 // Importation du générateur d'ID
 const uuid = require("uuid")
 
 const app = express()
+
+app.use(connection(mysql,connectOptions,"pool"))
 
 // Transformer le corps de la requête en json
 app.use(express.json())
@@ -22,39 +36,100 @@ app.get("/home",(req,res) => {
 // Définition des routes de l'API
 // Définir la route pour le traitement du formulaire
 app.post("/api/justify/:token", (req, res) => {
-    // Effectuer la vérification du nombre de mots utilisés. Si ce nombre de mots est > 80 000
-    // renvoyer une erreur 402 (Payment Required)
-    "SELECT * FROM users WHERE token = :token avec :token valant req.params.token"
-    if("requête if requête.words + text.words < 80 000"){
-        console.log(req.body)
-        "Faire justify sur req.body.text et le rendre dans le contenu"
-        // Traitement de justification
-        // res.header("Location",req.url).status(302).send("Texte justifié")
-        res.header("content-type","text/plain").status(200).send(req.body.text)
-        " else {"
-        "res.status(402).end()"
+    req.getConnection((error,connection) => {
+        if(error){
+            console.log(error)
+        }
+        else{
+        connection.query("SELECT * FROM users WHERE token = :token", [{":token": req.params.token}], (error,result) => {
+            if(error){
+                console.log(error)
+            }
+            else{
+                // "SELECT * FROM users WHERE token = :token avec :token valant req.params.token"
+                if(result){
+                    if(parseInt(result.words) + parseInt(req.body.text.length) < 80000){ /*"if requête.words + text.words < 80 000"*/
+                        // console.log(req.body)
+                        "Faire justify sur req.body.text et le rendre dans le contenu"
+                        // Traitement de justification
+                        // res.header("Location",req.url).status(302).send("Texte justifié")
+                        res.header("content-type","text/plain").status(200).send(req.body.text)
+                    }
+                    else{
+                        res.status(402).end()
+                    }
+                }
+                else{
+                    res.status(403).end()
+                }
+            }
+        })
     }
-    else{
-        "res.status(403).end()"
-    }
+    })
 
     // }
 })
 
 app.post("/api/token", (req, res) => {
-    // Si tu es fini vérifier que l'email est acceptable
-            "SELECT * FROM users WHERE email = :email"
-            "Oui: renvoyer {email: :email,token: :token, words: :words}" 
-            // Traitement de génération ou de récupération de token
-            "Non: INSERT INTO users(email,token,words) VALUES(:email,:token,0) renvoyer {email: :email,token: :token, words: 0}"
-        console.log(req.body)
-        res.header("content-type","application/json").send(req.body)//.cookie("token",token)
+            req.getConnection((error,connection) => {
+                if(error){
+                    console.log(error)
+                }
+                else{
+                connection.query("SELECT * FROM users WHERE email = :email", [{"email": req.params.email}], (error,result) => {
+                    if(error){
+                        console.log(error)
+                    }
+                    else{
+                        // "SELECT * FROM users WHERE token = :token avec :token valant req.params.token"
+                        if(result){
+                            data = {
+                                email: result.email,
+                                token: result.token,
+                                words: result.words,
+                            }
+                            console.log(data)
+                            data = JSON.stringify(data)
+                            res.header("content-type","application/json").send(req.body)
+                        }
+                        else{
+                            let continuing = true
+                            do{
+                                let token = uuid.v4()
+                                continuing = connection.query("SELECT * FROM users WHERE token = :token", [{"token": token}], (error,result) => {
+                                if(result){
+                                    return true
+                                }
+                                else{
+                                    return false
+                                }
+                                })
+                            }while(continuing)
+                            connection.query("INSERT INTO users(email,token,words) VALUES(:email,:token,0)", [{"email": req.params.email, "token": token}], (error,result) => {
+                                data = {
+                                    email: req.body.email,
+                                    token: token,
+                                    words: 0,
+                                }
+                                console.log(data)
+                                data = JSON.stringify(data)
+                                res.header("content-type","application/json").send(req.body)
+                            })
+                            res.status(403).end()
+                        }
+                    }
+                })
+            }
+            })
+        
+            
 })
 
-async () => {
-    await Date.time() === "00:00:00" 
-    "UPDATE users SET words = 0 WHERE "
-}
+// async () => {
+//     await Date.time() === "00:00:00" 
+//     "UPDATE users SET words = 0 WHERE "
+// }
+
 // Engager l'écoute des requêtes/ réponses faites au serveur
 app.listen(2500,() => {
     console.log("Initiation de l'écoute sur le port 2500")
--- justification.js
+++ justification.js
@@ -1,10 +1,62 @@
-function justify(texte){
-    
-    return [justifiedText,words]
-}
+
+// let word = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque corporis aliquid voluptatibus cupiditate id dolores 
+// praesentium provident sed. Quae ratione repellendus quisquam explicabo molestiae libero cum neque corrupti ullam laborum quam veniam 
+// animi sequi dolorem officiis, temporibus quis praesentium a? Odit debitis soluta possimus sint ut magnam molestias necessitatibus repellendus
+// molestiae praesentium. Facilis mollitia aperiam modi maiores a dolorum, nisi voluptates magnam possimus fuga aspernatur velit aut repellat 
+// exercitationem numquam repellendus nemo omnis officia explicabo architecto vel odio, quidem delectus illum. Dolores nulla atque quos 
+// illo officiis consectetur nam, molestias ipsum exercitationem omnis alias quisquam numquam debitis, amet aliquam qui vero sequi reprehenderit. 
+// Enim molestiae nisi ab exercitationem amet, quos odio voluptate ratione quo consequuntur tempore excepturi, veniam fuga? Vitae, doloremque 
+// officia? Earum odio, nemo cum ab laudantium qui sint, totam provident sed vitae architecto temporibus? Dolor iusto veniam consequatur qui ab nobis?
+// Sint tempora impedit hic illum deleniti ipsa obcaecati, molestias nobis modi error distinctio eveniet vitae libero sapiente porro nulla eius eum? 
+// Possimus non sunt sed nemo voluptatem magnam similique, labore eum, accusamus minus reiciendis laborum totam ullam excepturi minima ea ab nisi unde. 
+// Libero sed, velit tenetur eius laborum illo incidunt ratione dicta sapiente labore aut. Quae fugiat doloribus ex suscipit assumenda consequatur 
+// sint ipsa veniam, accusantium minima voluptates repellat quisquam id amet aperiam esse perspiciatis consequuntur quidem quo cupiditate sequi, 
+// sapiente delectus hic. Officiis maiores obcaecati unde harum. Itaque laudantium eum odit explicabo nostrum libero animi iste dolorum?`
+
+// function justify(texte){
+
+let word = `Longtemps, je me suis couché de bonne heure. Parfois, à peine ma bougie éteinte, mes yeux se fermaient si vite que je n’avais pas le temps de me dire: «Je m’endors.» Et, une demi-heure après, la pensée qu’il était temps de chercher le sommeil m’éveillait; je voulais poser le volume que je croyais avoir dans les mains et souffler ma lumière; je n’avais pas cessé en dormant de faire des réflexions sur ce que je venais de lire, mais ces réflexions avaient pris un tour un peu particulier; il me semblait que j’étais moi-même ce dont parlait l’ouvrage: une église, un quatuor, la rivalité de François Ier et de Charles-Quint. 
+
+Cette croyance survivait pendant quelques secondes à mon réveil; elle ne choquait pas ma raison, mais pesait comme des écailles sur mes yeux et les empêchait de se rendre compte que le bougeoir n’était plus allumé. 
+ Puis elle commençait à me devenir inintelligible, comme après la métempsycose les pensées d’une existence antérieure; le sujet du livre se détachait de moi, j’étais libre de m’y appliquer ou non; aussitôt je recouvrais la vue et j’étais bien étonné de trouver autour de moi une obscurité, douce et reposante pour mes yeux, mais peut-être plus encore pour mon esprit, à qui elle apparaissait comme une chose sans cause, incompréhensible, comme une chose vraiment obscure. Je me demandais quelle heure il pouvait être; j’entendais le sifflement des trains qui, plus ou moins éloigné, comme le chant d’un oiseau dans une forêt, relevant les distances, me décrivait l’étendue de la campagne déserte où le voyageur se hâte vers la station prochaine; et le petit chemin qu’il suit va être gravé dans son souvenir par l’excitation qu’il doit à des lieux nouveaux, à des actes inaccoutumés, à la causerie récente et aux adieux sous la lampe étrangère qui le suivent encore dans le silence de la nuit, à la douceur prochaine du retour.`
+
+let text = word.split("\n")
+let words = []
+text.forEach((element) => {
+    words.push(element.split(" "))
+});
+let line = []
+let wordsLength = 0;
+let paragraphe = []
+words.forEach(element => {
+    element.forEach((word) => {
+        if(word.length + wordsLength <= 80){
+            // console.log(word.length + wordsLength)
+            word+=" "
+            wordsLength+=word.length
+            line.push(word)
+        }
+        else{
+            paragraphe.push(line)
+            line = [word+" "]
+            wordsLength = word.length + 1
+        }
+    })
+    // console.log(line.join().length)
+});
+// console.log(text)
+console.log(paragraphe)
+
+// console.log("Longtemps, je me suis couché de bonne heure. Parfois, à peine ma bougie éteinte,".length)
+// console.log("mes  yeux se  fermaient si vite  que je  n’avais pas  le temps  de me  dire: «Je".length)
+
+    // return [justifiedText,words]
+// }
 
 // Faire en sorte d'enregistrer le token dans la session après connexion
 // Ne pas changer le token et mettre toutes les informations dans la session
 // Ne pas donner l'accès sans passer par l'authentification
 // Ne pas mettre le token dans l'URL mais dans la session ou dans les cookies
 // Pouvoir se déconnecter si on a pas rentré de texte
+
+
