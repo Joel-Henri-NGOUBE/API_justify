b907ec6a7001c5ed91434b93d2e054488275b370
Joël Henri <ebosqua.edimo@gmail.com>
2023-11-07 23:52:19 +0100

    Tests API 2

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ document.querySelector("input[type=submit]").addEventListener("click", () => {
     email = document.querySelector("input[type=text]").value
     fetch("/api/token",{
         method: "POST",
-        data: `{"email": "${email}"}`
+        body: `{"email": "${email}"}`
+    })
+    .then((response) => response.json())
+    .then((result) => {
+        if(result.length !== 0){
+            // fetch("api/justify")
+        }
     })
 })
